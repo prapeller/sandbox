87017882bf4180eeee1a6bb71f2b0b211a83fa07
prapeller <prapeller@mail.ru>
2021-12-07 11:38:01 +0300

    before less1

--- drf/library/authors/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
-from .models import Author
-
-
-class AuthorModelSerializer(ModelSerializer):
-    class Meta:
-        model = Author
-        fields = '__all__'
