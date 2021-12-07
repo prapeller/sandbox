49a5c13f797554e52bf62d262876ec671ee85af8
prapeller <prapeller@mail.ru>
2021-12-07 14:52:14 +0300

    finish less_01

---
+++ drf/hw/todolist/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64, unique=True)
